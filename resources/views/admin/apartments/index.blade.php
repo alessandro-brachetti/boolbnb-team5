a1efe5bf65c25950813a8bd3db5c389e6ee5bb1b
giuditta-marino <77673857+giuditta-marino@users.noreply.github.com>
2021-07-02 10:17:17 +0200

    font and index admin to complete

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -2,13 +2,20 @@
 @section('content')
 <main id="index-admin">
   <div class="container">
-    <div class="row ">
-
+    <div class="info row mpt-35">
+      <div class="title">
+        <h4>I miei appartamenti</h4>
+      </div>
+      <div class="dashboard">
+        <a href="{{route('dashboard')}}"><button type="button" name="button" class="white-btn">Torna alla dashboard</button></a>
+      </div>
+    </div>
+    <div class="apartments row mpt-35">
       @foreach(Auth::user()->apartments as $apartment)
-      <div class="col-lg-3 col-md-4 col-sm-6">
-        <div class="apartments d-flex flex-wrap">
-          <a href="{{route('admin.apartments.show', ['apartment'=> $apartment->id])}}" class="card-link">
-            <div class="card" style="width: 18rem;">
+      <div class="col-lg-3 col-md-4 col-sm-6 col-12">
+        <div class="d-flex flex-wrap">
+          <a href="{{route('admin.apartments.show', ['apartment'=> $apartment->id])}}">
+            <div class="card my-card mmb-30" style="width: 18rem;" title="Vedi i dettagli dell'appartamento">
               <div class="card-img-top my-card-img-top">
                 <img src="{{asset($apartment->img)}}" alt="">
               </div>
@@ -16,16 +23,18 @@
                 <h5 class="card-title my-card-title">{{$apartment->title}}</h5>
                 <div class="card-text my-card-text">
                   <p class="address">Indirizzo: {{$apartment->address}}</p>
-                  <p><span class="rooms">Stanze: {{$apartment->n_rooms}}</span> <span class="beds">Letti: {{$apartment->n_beds}}</span></p>
-                  <div class="actions">
-                    <div class="links">
-                      <a href="{{route('admin.apartments.edit', ['apartment'=>$apartment->id])}}" class="">Modifica</a>
+                  <p class="beds-rooms mpt-10"><span class="rooms">Stanze: {{$apartment->n_rooms}}</span> <span class="circle">&#183;</span> <span class="beds">Letti: {{$apartment->n_beds}}</span></p>
+
+                  <!-- azioni admin -->
+                  <div class="actions mpt-30">
+                    <div class="links col-4 text-center">
+                      <a href="{{route('admin.apartments.edit', ['apartment'=>$apartment->id])}}" title="Modifica"><i class="fas fa-edit"></i></a>
                     </div>
-                    <div class="links">
-                      <a href="{{route('admin.message.index', ['apartment'=>$apartment->id])}}" class="">Messaggi</a>
+                    <div class="links col-4 text-center">
+                      <a href="{{route('admin.message.index', ['apartment'=>$apartment->id])}}" title="Messaggi"><i class="fas fa-envelope-open-text"></i></a>
                     </div>
-                    <div class="links">
-                      <a href="{{route('admin.sponsor.index', ['apartment'=>$apartment->id])}}" class="">Sponsor</a>
+                    <div class="links col-4 text-center">
+                      <a href="{{route('admin.sponsor.index', ['apartment'=>$apartment->id])}}" title="Sponsorizza"><i class="fas fa-search-dollar"></i></a>
                     </div>
                   </div>
                 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,13 +17,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <!-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> -->
+    <!-- Fonts -->    
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Signika:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
